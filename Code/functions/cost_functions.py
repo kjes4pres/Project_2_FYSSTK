67ff80287bdc9f1b5e2ecc4f968d7628cb18b68f
Sverre M. J. <email@email.email>
2025-10-21 13:59:50 +0200

    FFNN is done, except Adam optimizer

--- Code/functions/cost_functions.py
+++ Code/functions/cost_functions.py
@@ -5,4 +5,5 @@ def mse(predict, target):
     return np.mean((predict - target) ** 2)
 
 def mse_der(predict, target):
+    assert predict.shape == target.reshape(-1,1).shape, "Not same"
     return 2 * (predict - target) / target.size
