16eba12c44bb1ce5e8b939064230020eac8c048f
Sverre M. J. <email@email.email>
2025-10-28 14:40:41 +0100

    cross-entropy

--- Code/functions/cost_functions.py
+++ Code/functions/cost_functions.py
@@ -7,3 +7,9 @@ def mse(predict, target):
 def mse_der(predict, target):
     assert predict.shape == target.reshape(-1,1).shape, "Not same"
     return 2 * (predict - target) / target.size
+
+def cross_entropy(predict, target):
+    return -np.sum(target*np.log(predict))
+
+def cross_entropy_der(predict, target):
+    return -target/predict
